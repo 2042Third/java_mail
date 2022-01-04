2bc464a835e28233daab7537563b0f388321bc89
Mike Yang <18604713262@163.com>
2022-01-04 14:51:31 -0500

    ok

--- util/SendMail.java
+++ util/SendMail.java
@@ -23,8 +23,8 @@ import javax.mail.Authenticator;
 
 public class SendMail {
 
-    protected String hostname = "outlook.office365.com";
-    // protected String hostname = "smtp-mail.outlook.com"; 
+    // protected String hostname = "outlook.office365.com";
+    protected String hostname = "smtp-mail.outlook.com"; 
     protected String username = "yangyimike@outlook.com"; 
     protected String password = "8b3ea0ce093993d50bf535627a156a737f5253611bf48c6f339e4a1984ab9882"; 
     protected Session session = null;
